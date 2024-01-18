c948a1ecf4924e5a364a82521a1489b084d7d40a
myengbaePark <pmb.dev3@gmail.com>
2024-01-18 17:37:53 +0900

    Spring

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,32 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    /**
+     *  구체화된 객체를 사용하지 않음으로서 OrderServiceImpl은 구체화된 객체에 의존하지 않게 됨(DIP)
+     *  다형성에 의해 member와 dicount는 외부에서 결정됨
+     *  AppConfig에 의해 구현체를 설정함
+      */
+    private final DiscountPolicy discountPolicy;
+    private final MemberRepository memberRepository;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int disCountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, disCountPrice);
+    }
+}
